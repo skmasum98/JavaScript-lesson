38a12fe1099d7983bb43233ed66525ed7c06d4d1
skmasum98 <skmasum98@gmail.com>
2025-05-27 00:03:57 +0600

    new

---
+++ day-16/index.js
@@ -0,0 +1,1 @@
+console.log("Day 16: JavaScript Debugging");
---
+++ expense-tracker/index.js
@@ -0,0 +1,16 @@
+// console.log("Expense Tracker");
+// Create a createExpenseTracker() function that takes a username and an initial budget to expose the following functioanlities:
+
+// Adding Expense
+// Removing Expense
+// Updating Expense
+// Getting total expenses done by the user
+// Getting expense by category
+// Get the Highest Expense
+// Get the Lowest Expense
+// Get the user info
+// Show all the expenses
+// Update User data
+// Please make use of the factory function, closure to keep data private and return only the required features/methods.
+
+
