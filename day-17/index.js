9c7d13d7359deac9a8d33fb399161d8a80007647
skmasum98 <skmasum98@gmail.com>
2025-05-31 17:17:53 +0600

    Day 17

---
+++ day-17/index.js
@@ -0,0 +1,41 @@
+console.log("Day 17 - 40 Days of JavaScript - Intro to JavaScript DOM");
+
+console.log(document);
+
+let titleElem = document.getElementById("heading");
+console.log(titleElem);
+
+let infoElems = document.getElementsByClassName("info");
+
+console.log(infoElems);
+console.log(infoElems[0].innerHTML);
+console.log(infoElems[1].innerHTML);
+
+
+[...infoElems].forEach((elem) => {
+    console.log(elem);
+});
+
+// By Tag Name:
+let pTagElems = document.getElementsByTagName("p");
+console.log(pTagElems); // HTMLCollection(3) [p.info, p.info, p]
+
+// ✅ querySelector:
+let para = document.querySelector("p.info");
+console.log("using query selector", para); // p.info first element
+
+// ✅ querySelectorAll:
+let paras = document.querySelectorAll("p.info");
+console.log("using query selector all", paras);
+
+
+// ✅ Changing the bg color 
+function highlightText() {
+    console.log("About to highlight a text...");
+    let elements = document.querySelectorAll("p.info");
+    elements.forEach((element) => {
+        element.style.backgroundColor = "yellow";
+    });
+}
+
+highlightText();
