cad92ec70984e8d3f7ae5de8ee5ecaddddc4c461
skmasum98 <skmasum98@gmail.com>
2025-06-06 10:11:32 +0600

    Day 18

--- day-18/index.js
+++ day-18/index.js
@@ -10,6 +10,46 @@ console.log(pElem);
 
 // insert element
 
-// insert before
+// insertBefore
 
-document.body.insertBefore(pElem, document.body.firstChild);
+const span = document.createElement("span");
+span.innerText = "আমি একটি span";
+const pElement = document.querySelector("p");
+document.body.insertBefore(span, pElement);
+
+// insert after
+const h2 = document.createElement("h2");
+h2.innerText = "আমি একটি h2";
+document.body.insertBefore(h2, pElement.nextElementSibling);
+
+// insert at the end
+const div = document.createElement("div");
+div.innerText = "আমি একটি div";
+document.body.insertBefore(div, null);
+
+
+// modify content
+{
+    //innerText vs innerHTML
+    const pElement2 = document.querySelector("p");
+    pElement2.innerText = "<u>This is a modified</u> paragraph using innerText.";
+
+    //it will not render HTML tags, it will show the text as is.
+    // <u>This is a modified</u> paragraph using innerText.
+
+    //innerHTML will render HTML tags
+    pElement2.innerHTML = "<u>This is a modified</u> paragraph using innerHTML.";
+}
+
+// remove element
+{
+    const list = document.getElementById("item-list");
+    const firstItem = list.children[0];
+    list.removeChild(firstItem); // remove first item from the list
+
+    const pElement = document.querySelector("p");
+    //list.replaceChildren(pElement); // সব children মুছে p যোগ
+
+   //document.getElementById("removeMe").remove(); // সরাসরি মুছে ফেলা
+
+}
